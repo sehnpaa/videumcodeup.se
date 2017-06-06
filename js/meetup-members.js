b0422909f7082f24d9e46823e281a3efbf7e08a3
Peter Andersson <pa222ku@student.lnu.se>
2017-06-06 17:44:37 +0000

    Add member pics

--- js/meetup-members.js
+++ js/meetup-members.js
@@ -1,16 +1,15 @@
-// Twine state object that will be populated with events
-var context = {};
-
 (() => {
   // Fetch meetup events json from glitch app
-  const api = 'https://videumcodeup-meetup-members.glitch.me'
+  const api = 'https://videum-codeup-meetup-members.glitch.me/members'
+  const defaultPic = 'https://secure.meetupstatic.com/s/img/6155798774261950966005/nobody_180.png';
 
   fetch(api)
     .then(response => response.json())
     .then(members => {
-      document.querySelector(".meetup-members-container").innerHTML =
-        members.reduce((string, member) => {
-	  return `${string}<div class="member"><a href="${member.link}"><img src="${member.thumb_link}" />${member.name}</a></div>`;
+      document.querySelector(".meetup-members-container").innerHTML = members.reduce((string, member) => {
+	  const pic = member.thumb_link ? member.thumb_link : defaultPic;
+	  return `${string}<div class="member"><a href="${member.link}"><img src="${pic}" />${member.name}</a></div>`;
 	}, "");
     });
+  document.querySelector(".meetup-members").classList.remove("hide");
 })()
